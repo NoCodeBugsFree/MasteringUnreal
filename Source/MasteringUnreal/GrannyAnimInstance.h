c0621638ff2ce92a0e4ce27af49d74643e6cb095
i5 <i5@I5-PC>
2017-05-27 11:16:24 +0300

    Course End

---
+++ Source/MasteringUnreal/GrannyAnimInstance.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "GrannyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTERINGUNREAL_API UGrannyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
